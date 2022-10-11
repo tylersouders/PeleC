1737bde8e71f6aee7d3ba19038bfc66139b14cd9
Tyler Souders <tsouders@login4.frontera.tacc.utexas.edu>
2022-10-11 18:37:10 -0500

    minor compile fix

--- Exec/TJS/Reiker_URampUp/prob.cpp
+++ Exec/TJS/Reiker_URampUp/prob.cpp
@@ -116,7 +116,7 @@ extern "C" {
     }
 
     // Save this Mach number
-    prob_parm.Mach_inlet = M;
+    // prob_parm.Mach_inlet = M;
 
     // Use Total-to-Static to get actual inlet conditions!
     PeleC::h_prob_parm_device->p_in = PeleC::h_prob_parm_device->p0 / 
